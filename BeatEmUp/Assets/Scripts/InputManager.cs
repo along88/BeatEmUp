b2f3dcfbd3626a1a9a3ca0c0d72a83483a6333a5
along88 <a.long88@yahoo.com>
2017-01-19 10:00:49 -0500

    InputManager

---
+++ BeatEmUp/Assets/Scripts/InputManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager instance; //singleton
+    private InputManager() { } //Constructor
+    public static InputManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+              instance = new InputManager();
+            }
+            return instance;     
+        }
+    }
+
+    public float Horizontal()
+    {
+        return Input.GetAxisRaw("Horizontal");
+    }
+    public float Vertical()
+    {
+        return Input.GetAxisRaw("Vertical");
+    }
+    public Vector2 Movement()
+    {
+        return new Vector2(Horizontal(),Vertical());
+    }
+    public bool Jump()
+    {
+        return Input.GetButtonDown("Jump");
+    }
+    public bool HasJumped()
+    {
+        return Input.GetButtonUp("Jump");
+    }
+    public bool Attack()
+    {
+        return Input.GetButtonDown("Attack");
+    }
+}
---
+++ BeatEmUp/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed;
+    private float minJumpHeight;
+    private float maxJumpHeight;
+    private float jumpTime;
+    public float gravity;
+    private float minJumpVelocity;
+    private float maxJumpVelocity;
+    private float velocityXMovement;
+    private float airAccelerationTime;
+    private float accelerationGroundTime;
+    private Vector2 input;
+    private Vector2 velocity;
+    private bool canMove;
+    private Rigidbody2D RB;
+
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        speed = 3.0f;
+        minJumpHeight = 0.2f;
+        maxJumpHeight = 1.00f;
+        jumpTime = 0.4f;
+        airAccelerationTime = 0.2f;
+        accelerationGroundTime = 0.1f;
+        canMove = true;
+
+        gravity = -(2.0f * maxJumpHeight) / Mathf.Pow(jumpTime, 2.0f);
+        minJumpVelocity = Mathf.Sqrt(2.0f * Mathf.Abs(gravity) * minJumpHeight);
+        maxJumpVelocity = Mathf.Abs(gravity) * jumpTime; 
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(canMove)
+        {
+           velocity = InputManager.Instance.Movement();
+            RB.velocity = new Vector2(velocity.x * speed, velocity.y * speed);
+        }
+    }
+    
+}
